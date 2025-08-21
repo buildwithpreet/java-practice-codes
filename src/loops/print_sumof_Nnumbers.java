3c1d17cff4deb94ab26ee9f3c43215fba61d388a
preetnagar761 <preetnagar498@gmail.com>
2025-08-21 18:53:10 +0530

    new code of loops updated in src

---
+++ src/loops/print_sumof_Nnumbers.java
@@ -0,0 +1,17 @@
+package loops;
+
+import java.util.Scanner;
+
+public class print_sumof_Nnumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while(i<=n){
+        sum = sum + i;
+        i++;
+
+        } System.out.println(sum);
+    }
+}
