f5f433d9ad6b333d7e910dc0ee4bded25b2ea28a
preetnagar761 <preetnagar498@gmail.com>
2025-08-22 21:37:08 +0530

    update

--- src/loops/forloop/print_reverse_of_a_number.java
+++ src/loops/forloop/print_reverse_of_a_number.java
@@ -1,5 +1,14 @@
 package loops.forloop;
 
+import java.util.Scanner;
+
 public class print_reverse_of_a_number {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int rev = 0;
+
+
+    }
 
 }
