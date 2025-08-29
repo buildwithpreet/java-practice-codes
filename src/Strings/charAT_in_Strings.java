d6dc42aaa23517b1706ff4445afadf378b416408
preetnagar761 <preetnagar498@gmail.com>
2025-08-29 22:52:31 +0530

    updated todays code

--- src/Strings/charAT_in_Strings.java
+++ src/Strings/charAT_in_Strings.java
@@ -1,5 +1,18 @@
 package Strings;
 
 public class charAT_in_Strings {
+    public static void printCharAt(String str) {
+        for (int i=0; i<str.length(); i++) {
+            System.out.print(str.charAt(i)+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main (String[] args) {
+        String s1 = new String("PREET");
+        String s2 = new String("DHAKAD");
+        String fullname = s1+" "+s2;
+        printCharAt(fullname);
+    }
     
 }
